7e9005ed304cee46050d2fe3ca297bff1e64d1ff
Aamir-K11 <amirkhan11999@gmail.com>
2022-07-03 22:29:05 +0500

    (Feat): Added schema for to-do list item

---
+++ schemas/to-do.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const ToDoSchema = new mongoose.Schema({
+   
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    description: {
+        type: String,
+        maxLength: 100,
+        trim: true
+    },
+
+    priority: {
+        type: String,
+        enum: ['HIGH', 'MEDIUM', 'LOW']
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+
+
+});
+
+const ToDo = new mongoose.model('ToDo', ToDoSchema);
+
+module.exports = ToDo;
