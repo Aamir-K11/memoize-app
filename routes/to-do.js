d30f534165661a0bc6553faa64dc5781db82d856
Aamir-K11 <amirkhan11999@gmail.com>
2022-07-19 22:41:34 +0500

    added todo task creation route

--- routes/to-do.js
+++ routes/to-do.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router  = express.Router();
-const {ToDo}    = require('../schemas/to-do');
+const {ToDo}  = require('../schemas/to-do');
+const {ToDoList}  = require('../schemas/to-do-list');
+const User    = require('../schemas/user');  
 const JwtAuth  = require('../middleware/jwt-auth');
+const { BadRequestError } = require('../errors');
 require('express-async-errors');
 
-router.post('/', async (req, res) => {
+router.post('/', [JwtAuth], async (req, res) => {
 
     const ToDoObject = {
         title: req.body.title,
@@ -12,8 +15,17 @@ router.post('/', async (req, res) => {
         priority: req.body.priority,
     };
 
-    const ToDoDocument = await ToDo.create(ToDoObject);
-    return res.send(ToDoDocument);
+    const toDoDocument = await ToDo.create(ToDoObject);
+    const user = await User.findOne({_id: req.user.user_id});
+
+    if(!user.todolist) throw new BadRequestError('No todo list exists for this user');
+
+    const todolist = await ToDoList.updateOne({_id: user.todolist}, {$push: {todos: toDoDocument}});
+
+    if(todolist.modifiedCount > 0)   return res.send('ToDo list has been updated');
+
+    return res.send('ToDo list has not been updated');
+  
 });
 
 router.get('/', [JwtAuth], async (req, res) => {
