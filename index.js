736b61f74bc6d3f9f2fd3cb0efd3819d27672f5e
Aamir-K11 <amirkhan11999@gmail.com>
2022-07-23 21:28:46 +0500

    added error route to index

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ require('./startup/database')()
 
 const app = express()
 require('./startup/routes')(app)
-
+app.all('*', (req, res) => res.send('<h1>Error404! Page not found!<h1>'))
 app.listen(process.env.PORT || 8000, () => { console.log('Listening on port 5000') })
