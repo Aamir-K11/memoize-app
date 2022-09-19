7969b031c78846b5979b1232bfbcb51d8ff5fce9
Aamir-K11 <amirkhan11999@gmail.com>
2022-09-19 22:43:58 +0500

    added prod script

--- createApp.js
+++ createApp.js
@@ -7,7 +7,7 @@ require('./startup/database')()
 const createApp = () => {
   const app = express()
 
-  if (process.env.NODE_ENV == 'production') {
+  if (process.env.NODE_ENV == 'prod') {
     app.use(cors())
     app.use(helmet())
   }
