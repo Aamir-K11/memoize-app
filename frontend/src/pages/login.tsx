012ff45380cc96db4c4bc4c87661bc05734b4c1c
Aamir-K11 <amirkhan11999@gmail.com>
2022-12-27 22:14:25 +0500

    added buttoon and made changes to login

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,12 +1,15 @@
 import Button from "../components/button";
+import classes from "./login.module.css";
 
 const Login = () => {
     return (
-    <form>
-        <input type="email" id="email" name="email"></input>
-        <input type="password" id="password" name="password"></input>
-        <Button text="Login" onClick={(event: React.MouseEvent<HTMLButtonElement>) => console.log("Login") }/>
-    </form>);
+    <div className={classes['login-container']}>
+        <form className={classes['login-form']}>
+            <input type="email" id="email" name="email"></input>
+            <input type="password" id="password" name="password"></input>
+            <Button text="Login" onClick={(event: React.MouseEvent<HTMLButtonElement>) => console.log("Login") }/>
+        </form>
+    </div>);
 }
 
 export default Login;
