923bd007f85998cafc4760e8dc46691433305517
Aamir-K11 <amirkhan11999@gmail.com>
2023-02-02 02:01:56 +0500

    feat: added login submit handler in login form

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -3,19 +3,29 @@ import classes from "./form.module.css";
 import loginSchema from "../schemas/login";
 import { yupResolver } from '@hookform/resolvers/yup';
 import Input from "../components/input";
+import axios from "axios";
 
 interface LoginFormInput {
     Email: string;
     Password: string;
 };
 
-const onSubmitHandler: SubmitHandler<LoginFormInput> = (data) => console.log(data);
-
 const Login = () => {
     const { register, handleSubmit, formState: { errors } } = useForm<LoginFormInput>({
         resolver: yupResolver(loginSchema)
     }   
     );
+
+    const onSubmitHandler: SubmitHandler<LoginFormInput> = (data) => {
+        axios.post('http://localhost:8000/login', {
+            email: data.Email,
+            password: data.Password
+        }).then((res) => {
+            console.log(res);
+        }).catch((err)=>{
+            console.log(err.message);
+        })
+    };
 
     return (
         <form onSubmit = {handleSubmit(onSubmitHandler)} className={classes['__form']}>
