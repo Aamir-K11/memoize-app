d73cfe2692ec9ecea392bc395d83df90aa5e765f
Aamir-K11 <amirkhan11999@gmail.com>
2023-02-25 19:28:07 +0500

    feat: styled welcome page

--- frontend/src/pages/welcome/welcome.tsx
+++ frontend/src/pages/welcome/welcome.tsx
@@ -1,5 +1,16 @@
+import classes from "./welcome.module.css";
+import welcome from "../../assets/welcome.png";
+import { Link } from "react-router-dom";
+import Button from "../../components/button/button";
+
 const Welcome = () => {
-    return <h1>Memoize</h1>
+    return (<div className={classes.main}>
+        <img src={welcome} className={classes.welcome}/> 
+        <div className={classes["btn-group"]}>
+             <Link to="/login"><Button text="Login" className={classes["custom-welcome-btn"]}/></Link>
+             <Link to="/signup"><Button text="Signup" className={classes["custom-welcome-btn"]}/></Link>
+        </div>
+    </div>)
 }
 
 export default Welcome;
