5047da67d0b44074c626610b42ee4bb47d3273c2
Aamir-K11 <amirkhan11999@gmail.com>
2023-02-28 19:18:21 +0500

    feat: added protected route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,7 @@
+import React from "react"
 import { Route, Routes } from "react-router"
+import { AuthContextType } from "./@types/auth"
+import { AuthContext } from "./context/auth-context"
 import Layout from "./layout/layout"
 import NotFound from "./pages/404/404"
 import Dashboard from "./pages/dashboard"
@@ -6,8 +9,11 @@ import Login from "./pages/login"
 import SignUp from "./pages/signup"
 import VerifyUser from "./pages/verifyuser"
 import Welcome from "./pages/welcome/welcome"
+import ProtectedRoute from "./utils/protectedRoute"
 
 function App() {
+  
+  const {user} = React.useContext(AuthContext) as AuthContextType;
   
   return (
     <Layout>
@@ -15,7 +21,11 @@ function App() {
         <Route path="/" element={<Welcome/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/dashboard" element={
+          <ProtectedRoute user={user} redirectPath="/login">
+              <Dashboard/>
+          </ProtectedRoute>
+        }/>
         <Route path="/verify" element={<VerifyUser/>}/>
         <Route path="/*" element={<NotFound/>}/>
       </Routes>
