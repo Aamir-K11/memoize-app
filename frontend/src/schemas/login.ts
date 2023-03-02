3744ffdac9d727d83a737cd0e138420bec14be4b
Aamir-K11 <amirkhan11999@gmail.com>
2023-03-02 22:56:38 +0500

    fix: added min password check

--- frontend/src/schemas/login.ts
+++ frontend/src/schemas/login.ts
@@ -2,7 +2,7 @@ import { object, string } from 'yup';
 
 const loginSchema = object().shape({
   Email: string().email().required("Email is required"),
-  Password: string().required("Password is required")
+  Password: string().min(8, 'Password length should not be less than 8').required("Password is required")
 });
 
 export default loginSchema;
