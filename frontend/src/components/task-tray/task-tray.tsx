a969c4bc6cb665e0477832068977559792079425
Aamir-K11 <amirkhan11999@gmail.com>
2023-03-08 23:23:18 +0500

    fix: moved tasks fetch logic to task tray

--- frontend/src/components/task-tray/task-tray.tsx
+++ frontend/src/components/task-tray/task-tray.tsx
@@ -1,7 +1,31 @@
+import axios from "axios";
+import React from "react";
+import { useState, useEffect } from "react";
+import { AuthContextType } from "../../@types/auth";
 import Task from "../tasks/tasks";
 import classes from './task-tray.module.css';
+import { AuthContext } from "../../context/auth-context";
+import { ToDo } from "../../@types/todo";
 
 const TaskTray = () => {
+
+    const {user} = React.useContext(AuthContext) as AuthContextType;
+    
+    const [tasks, setTasks] = useState<ToDo[]>([]);
+
+    useEffect(() => {
+        axios
+        .get('http://localhost:5000/todo', {
+            headers: {
+                'x-access-token' : user?.JWTtoken
+            }
+        })
+        .then(tasks=> {
+            setTasks(tasks.data.todos)
+        })
+        .catch(err => console.log(err))
+      }, []);
+
     return (
         <div className={classes['__task-tray']}>
             <Task/>
