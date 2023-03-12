3ffb9e5a7b8fdcda4d8e16a776d13b3c34f2f944
Aamir-K11 <amirkhan11999@gmail.com>
2023-03-12 23:28:34 +0500

    fix: spacing in component

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -5,6 +5,8 @@ import { Link, useLocation } from 'react-router-dom';
 import React from 'react';
 import { AuthContext } from '../../context/auth-context';
 import { AuthContextType } from '../../@types/auth';
+
+
 const Header = () => {
     
     const URL = useLocation();
